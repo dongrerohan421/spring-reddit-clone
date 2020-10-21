e4e3ed9edc489d6a3f7e35f0928ac07d9a59dffb
dongrerohan421 <dongrerohan421@gmail.com>
2020-10-21 14:41:11 -0400

    add Post entity

---
+++ src/main/java/com/springredditclone/model/Post.java
@@ -0,0 +1,54 @@
+package com.springredditclone.model;
+
+import java.time.Instant;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotBlank;
+
+import org.springframework.lang.Nullable;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long postId;
+
+    @NotBlank(message = "Post Name cannot be empty or Null")
+    private String postName;
+
+    @Nullable
+    private String url;
+
+    @Nullable
+    @Lob
+    private String description;
+
+    private Integer voteCount;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id", referencedColumnName = "id")
+    private User user;
+
+    private Instant createdDate;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id", referencedColumnName = "id")
+    private Subreddit subreddit;
+}
