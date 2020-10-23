d4a82653cfed7ba7abc16d9a115d020b158b0c83
dongrerohan421 <dongrerohan421@gmail.com>
2020-10-23 19:14:41 -0400

    Create Endpoint to Verify Users

--- src/main/java/com/springredditclone/controller/AuthController.java
+++ src/main/java/com/springredditclone/controller/AuthController.java
@@ -5,6 +5,8 @@ import com.springredditclone.service.AuthService;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,4 +27,9 @@ public class AuthController {
         return new ResponseEntity<>("User Registration Successful", HttpStatus.OK);
     }
 
+    @GetMapping("accountVerification/{token}")
+    public ResponseEntity<String> verifyAccount(@PathVariable String token) {
+        authService.verifyAccount(token);
+        return new ResponseEntity<>("Account Activated Successully", HttpStatus.OK);
+    }
 }
