3f6ed16a3e6e2559f2de73939322a776f75fe97b
dongrerohan421 <dongrerohan421@gmail.com>
2020-10-22 21:25:06 -0400

    create VerificationTokenRepository

---
+++ src/main/java/com/springredditclone/repository/VerificationTokenRepository.java
@@ -0,0 +1,13 @@
+package com.springredditclone.repository;
+
+import java.util.Optional;
+
+import com.springredditclone.model.VerificationToken;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VerificationTokenRepository extends JpaRepository<VerificationToken, Long> {
+    Optional<VerificationToken> findByToken(String token);
+}
