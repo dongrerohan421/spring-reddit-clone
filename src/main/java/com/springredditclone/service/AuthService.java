a1dc53e8de0021a9fa461b0a54ef6ef95d3eb473
dongrerohan421 <dongrerohan421@gmail.com>
2020-10-23 19:07:19 -0400

    Activating new account via email and Using MailTrap to send emails

--- src/main/java/com/springredditclone/service/AuthService.java
+++ src/main/java/com/springredditclone/service/AuthService.java
@@ -1,8 +1,13 @@
 package com.springredditclone.service;
 
 import com.springredditclone.dto.RegisterRequest;
+import com.springredditclone.exceptions.SpringRedditException;
+import com.springredditclone.model.NotificationEmail;
 import com.springredditclone.model.User;
+import com.springredditclone.model.VerificationToken;
 import com.springredditclone.repository.UserRepository;
+import com.springredditclone.repository.VerificationTokenRepository;
+import com.springredditclone.util.Constants;
 
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -12,12 +17,18 @@ import lombok.AllArgsConstructor;
 
 import static java.time.Instant.now;
 
+import java.util.Optional;
+import java.util.UUID;
+
 @Service
 @AllArgsConstructor
 public class AuthService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final VerificationTokenRepository verificationTokenRepository;
+    private final MailContentBuilder mailContentBuilder;
+    private final MailService mailService;
 
     @Transactional
     public void signup(RegisterRequest registerRequest) {
@@ -31,9 +42,43 @@ public class AuthService {
         user.setEnabled(false);
 
         userRepository.save(user);
+
+        String token = generateVerificationToken(user);
+        String message = mailContentBuilder.build(
+                "Thank you for signing up to Spring Reddit, please click on the below url to activate your account : "
+                        + Constants.ACTIVATION_EMAIL + "/" + token);
+
+        mailService.sendMail(new NotificationEmail("Please Activate your account", user.getEmail(), message));
+    }
+
+    private String generateVerificationToken(User user) {
+
+        String token = UUID.randomUUID().toString();
+        VerificationToken verificationToken = new VerificationToken();
+        verificationToken.setToken(token);
+        verificationToken.setUser(user);
+
+        verificationTokenRepository.save(verificationToken);
+        return token;
     }
 
     private String encodePassword(String password) {
         return passwordEncoder.encode(password);
+    }
+
+    public void verifyAccount(String token) {
+        Optional<VerificationToken> verificationTokenOptional = verificationTokenRepository.findByToken(token);
+
+        fetchUserAndEnable(verificationTokenOptional.orElseThrow(() -> new SpringRedditException("Invalid Token")));
+    }
+
+    private void fetchUserAndEnable(VerificationToken verificationToken) {
+        String userName = verificationToken.getUser().getUserName();
+        User user = userRepository.findByUserName(userName)
+                .orElseThrow(() -> new SpringRedditException("User Not Found with id - " + userName));
+
+        user.setEnabled(true);
+
+        userRepository.save(user);
     }
 }
---
+++ src/main/java/com/springredditclone/service/MailService.java
@@ -0,0 +1,40 @@
+package com.springredditclone.service;
+
+import com.springredditclone.exceptions.SpringRedditException;
+import com.springredditclone.model.NotificationEmail;
+
+import org.springframework.mail.MailException;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
+import org.springframework.stereotype.Service;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@AllArgsConstructor
+@Slf4j
+public class MailService {
+
+    private final JavaMailSender mailSender;
+    private final MailContentBuilder mailContentBuilder;
+
+    void sendMail(NotificationEmail notificationEmail) {
+        MimeMessagePreparator messagePreparator = mimeMessage -> {
+            MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
+            messageHelper.setFrom("springreddit@email.com");
+            messageHelper.setTo(notificationEmail.getRecipient());
+            messageHelper.setSubject(notificationEmail.getSubject());
+            messageHelper.setText(mailContentBuilder.build(notificationEmail.getBody()));
+        };
+
+        try {
+            mailSender.send(messagePreparator);
+            log.info("Activation Email sent!!");
+        } catch (MailException e) {
+            throw new SpringRedditException(
+                    "Exception occurred when sending mail to " + notificationEmail.getRecipient());
+        }
+    }
+}
